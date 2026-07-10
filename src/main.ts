c65466f202a037121bfa74760b397e21ebd76a4d
sak13301 <sak13301@esri.com>
2026-07-10 17:45:02 +0530

    improvement in styling and added card ui

--- src/main.ts
+++ src/main.ts
@@ -5,26 +5,27 @@ import "./resultCard";
 import { getIPdata } from "./getIPdata";
 
 document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
+<div class="app flex flex-col h-screen">
 <div id="header">
   <div class="text-white text-4xl py-[2rem]">IP Address tracker</div>
   
-  <search-bar class="block"></search-bar>
+  <search-bar ></search-bar>
 
   <result-card></result-card>
 
 
   
 </div>
-<section id="map">
+<section id="map" class="grow">
  <iframe class="map-top"
     loading="lazy"
     referrerpolicy="strict-origin-when-cross-origin"
     src="https://www.google.com/maps/embed/v1/place?key=${import.meta.env.VITE_GOOGLE_API_KEY}
     &q=37.4220,-122.0841" allowfullscreen></iframe>
 </section>
+</div>
 
-<div class="ticks"></div>
-<section id="spacer"></section>
+
 `;
 
 console.log(getIPdata("8.8.8.8"));
--- src/searchBar.ts
+++ src/searchBar.ts
@@ -1,7 +1,7 @@
 class searchBarComp extends HTMLElement {
   connectedCallback() {
     this.innerHTML = `
-    <input type="text" name="search-bar" class="bg-white p-[1rem]  min-w-[450px] rounded-s-2xl" placeholder="Search for any IP address or domain"/><button class="search-btn py-[1rem] px-[1.4rem] font-bold bg-black text-white rounded-e-2xl cursor-pointer">
+    <input type="text" name="search-bar" class="bg-white p-[1rem] relative  min-w-[450px] rounded-s-2xl" placeholder="Search for any IP address or domain"/><button class="search-btn py-[1rem] px-[1.4rem] font-bold bg-black text-white rounded-e-2xl cursor-pointer">
     <img src="../images/icon-arrow.svg" class="inline" alt=">"/>
     </button>
     `;
